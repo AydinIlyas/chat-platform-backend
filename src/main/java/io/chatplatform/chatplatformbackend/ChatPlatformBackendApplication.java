ed43a6753c68afd9c76ec8cbff543defce9f3bbe
Ilyas Aydin <ilyasaydin55sa@gmail.com>
2025-08-02 17:24:12 +0300

    Initial commit

---
+++ src/main/java/io/chatplatform/chatplatformbackend/ChatPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package io.chatplatform.chatplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/chatplatform/chatplatformbackend/ChatPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chatplatform.chatplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
